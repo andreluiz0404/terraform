f9092b753335929c5650e90dc92b9200c8c7708e
André Luiz Ferreira Dias <andre@eznetservices.com>
2023-01-25 18:23:32 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Projeto construção de infraestrutura
+
+### Sobre o projeto
+Este projeto constrói a infraestrutura no Digital Ocean Cloud Provider utilizando a ferramenta Terraform e HCL (HashiCorp Configuration Language).
---
+++ main.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+variable "do_token" {
+  default = ""
+}
+
+variable "region" { }
+
+variable "ssh_key_name" { }
+
+data "digitalocean_ssh_key" "ssh_key" {
+  name = var.ssh_key_name
+}
+
+# Cria o droplet para o Jenkins da aula 4
+resource "digitalocean_droplet" "jenkins" {
+  image    = "ubuntu-22-04-x64"
+  name     = "jenkins"
+  region   = var.region
+  size     = "s-2vcpu-2gb"
+  ssh_keys = [data.digitalocean_ssh_key.ssh_key.id]
+}
+
+# Cria o cluster kubernetes
+resource "digitalocean_kubernetes_cluster" "k8s" {
+  name    = "k8s"
+  region  = var.region
+  version = "1.25.4-do.0"
+
+  node_pool {
+    name       = "default"
+    size       = "s-2vcpu-2gb"
+    node_count = 2
+  }
+}
+
+# Cria o resource local_file
+resource "local_file" "kube_config" {
+  content = digitalocean_kubernetes_cluster.k8s.kube_config.0.raw_config
+  filename = "kube_config.yaml"
+}
+
+output "jenkins_ip" {
+	value = digitalocean_droplet.jenkins.ipv4_address
+}
